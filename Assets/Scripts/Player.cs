cab37d3b7f2be9e77f15ccd4dd46528d864a3cba
fz <fzaers@gmail.com>
2019-06-19 15:00:00 +0800

    Added Player resources

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animation thisAnimation;
+
+    void Start()
+    {
+        thisAnimation = GetComponent<Animation>();
+        thisAnimation["Flap_Legacy"].speed = 3;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            thisAnimation.Play();
+    }
+}
